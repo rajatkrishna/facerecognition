38fab4460aca77af21bd2824012dde423f4429b5
Rajat U Krishna <rajatkrishna99@gmail.com>
2020-05-22 23:13:37 +0530

    minor bug fixes

--- face_det.py
+++ face_det.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import dlib
 
-image_file = "images/20200520_094328.jpg"
+image_file = "images/download.jpeg"
 
 def rect_to_bb(rect):
     x = rect.left()
@@ -16,15 +16,17 @@ def rect_to_bb(rect):
 
 def display(img, n, bb_points):
     
-    img_rect = img.copy()
+    img_rect = np.copy(img)
     for i in range(n):
         x, y, w, h = bb_points[i]
         r, g, b = np.random.choice(256, 3)
-        img_rect = cv2.rectangle(img_rect, (x, y), (x + w, y + h), (r, g, b))
+        img_rect = cv2.rectangle(img_rect, (x, y), (x + w, y + h), (r.item(), g.item(), b.item()), thickness = 2)
     
-    cv2.imshow('Detected faces', img_rect)
+    img_rect = cv2.cvtColor(img_rect, cv2.COLOR_BGR2RGB)
+    cv2.imshow("Detected faces", img_rect)
+    cv2.waitKey()
 
-img = cv2.imread(img_file)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+img = cv2.imread(image_file)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 img_copy = img.copy()
 img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
@@ -32,15 +34,16 @@ img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
 face_detector = dlib.get_frontal_face_detector()
 
-faces, scores, idx = detector.run(gray, 1, 0.25)
+faces, scores, idx = face_detector.run(img, 1, 0.25)
 
-print("{} faces found in image {}".format(len(faces), i + 1))
+print("{} faces found in image {}".format(len(faces), image_file))
 bb_points = []
 for face_rect in faces:
     points = rect_to_bb(face_rect)
     bb_points.append(points)
 
-display(img_copy, len(faces), bb_points)
+if len(faces) > 0:
+    display(img_copy, len(faces), bb_points)
 
     
 
